48bd0bb21ca28bb920d89bf502f7b690726b50a1
Leonardo Ascione <leonardo.ascione@gmail.com>
2019-03-27 01:21:54 +0100

    🏗 Add example code to test InversifyJS property injector
    In order to work on issue #2

---
+++ examples/inversify/src/index.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+
+import getDecorators from 'inversify-inject-decorators';
+import { Container, injectable } from 'inversify';
+
+@injectable()
+class PrintService {
+  print(book: Book) {
+    console.log(`Book "${book.summary}" by ${book.author}.`);
+  }
+}
+
+let container = new Container();
+container.bind<PrintService>('PrintService').to(PrintService);
+let { lazyInject } = getDecorators(container);
+
+class Book {
+  @lazyInject('PrintService')
+  private printService!: PrintService;
+
+  public constructor(public author: string, public summary: string) {}
+
+  public print() {
+    this.printService.print(this);
+  }
+}
+
+// Book instance is NOT created by InversifyJS
+let book = new Book('George Orwell', '1984');
+book.print();
